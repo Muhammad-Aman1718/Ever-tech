621b3be3eb1e671f62fd220712162a096f7c6fa1
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-04 12:18:57 +0500

    Completed

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // process.env.NEXTAUTH_URL
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", // API Base URL
+  baseURL: process.env.NEXTAUTH_URL, // API Base URL
   timeout: 50000, // 10 seconds timeout
   headers: {
     "Content-Type": "application/json",
